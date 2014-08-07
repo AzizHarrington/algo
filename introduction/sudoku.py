03512ccc4b54527593eb3703d1a6b858bb042346
azizharrington <azizharrington@gmail.com>
2014-08-07 15:20:20 -0500

    switched to 2d array

--- introduction/sudoku.py
+++ introduction/sudoku.py
@@ -1,23 +1,24 @@
 puzzle = [
-    '159*6*7**',
-    '**71**49*',
-    '*84*9****',
-    '**59***8*',
-    '8*2*3*9*4',
-    '*3***25**',
-    '****5*14*',
-    '*96**38**',
-    '**8*4*632'
+    [1, 5, 9, 0, 6, 0, 7, 0, 0, 0],
+    [0, 0, 7, 1, 0, 0, 4, 9, 0, 0],
+    [0, 8, 4, 0, 9, 0, 0, 0, 0, 0],
+    [0, 0, 5, 9, 0, 0, 0, 8, 0, 0],
+    [8, 0, 2, 0, 3, 0, 9, 0, 4, 0],
+    [0, 3, 0, 0, 0, 2, 5, 0, 0, 0],
+    [0, 0, 0, 0, 5, 0, 1, 4, 0, 0],
+    [0, 9, 6, 0, 0, 3, 8, 0, 0, 0],
+    [0, 0, 8, 0, 4, 0, 6, 3, 2, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ]
 
 
 def check_row(row, puzzle):
-    digits = [n for n in puzzle[row] if n.isdigit()]
+    digits = [n for n in puzzle[row] if n]
     return len(digits) == len(set(digits))
 
 
 def check_column(column, puzzle):
-    digits = [n[column] for n in puzzle if n[column].isdigit()]
+    digits = [n[column] for n in puzzle if n[column]]
     return len(digits) == len(set(digits))
 
 
@@ -30,25 +31,27 @@ def check_square(square, puzzle):
     if square in [6, 7, 8]:
         select_rows = puzzle[6:]
     index = square%3
-    gen = [(n for n in row[index*3:(index+1)*3] if n.isdigit())
+    gen = [(n for n in row[index*3:(index+1)*3] if n)
             for row in select_rows]
     digits = list(itertools.chain(*[list(n) for n in gen]))
     return len(digits) == len(set(digits))
 
 
+test_puzzle = [
+    [1, 5, 9, 0, 6, 0, 7, 0, 0, 0],
+    [0, 7, 7, 1, 0, 0, 4, 9, 0, 0],
+    [0, 8, 4, 0, 9, 0, 0, 9, 0, 0],
+    [0, 0, 5, 9, 0, 0, 0, 8, 0, 0],
+    [8, 0, 2, 0, 3, 0, 9, 0, 4, 0],
+    [0, 3, 0, 0, 0, 2, 5, 0, 0, 0],
+    [0, 0, 0, 0, 5, 0, 1, 4, 0, 0],
+    [0, 9, 6, 0, 0, 3, 8, 0, 0, 0],
+    [0, 0, 8, 0, 4, 0, 6, 3, 2, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
 
 def test():
-    test_puzzle = [
-        '159*6*7**',
-        '*771**49*',
-        '*84*9**9*',
-        '**59***8*',
-        '8*2*3*9*4',
-        '*3***25**',
-        '****5*14*',
-        '*96**38**',
-        '**8*4*632'
-    ]
     assert check_row(0, test_puzzle) == True
     assert check_row(1, test_puzzle) == False
     assert check_column(8, test_puzzle) == True
@@ -56,6 +59,8 @@ def test():
     assert check_square(6, test_puzzle) == True
     assert check_square(0, test_puzzle) == False
     print('tests passed')
+    return True
 
+if not test():
+    test()
 
-print(test())
