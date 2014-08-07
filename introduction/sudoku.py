f88a00e14f8246550477dc71331336b232adb6a6
azizharrington <azizharrington@gmail.com>
2014-08-07 16:06:09 -0500

    solve_row starter function

--- introduction/sudoku.py
+++ introduction/sudoku.py
@@ -14,6 +14,7 @@ puzzle = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ]
 
+
 def check_row(row, puzzle):
     digits = [n for n in puzzle[row] if n]
     return len(digits) == len(set(digits))
@@ -36,6 +37,21 @@ def check_square(square, puzzle):
             for row in select_rows]
     digits = list(itertools.chain(*[list(n) for n in gen]))
     return len(digits) == len(set(digits))
+
+
+def solve_row(puzzle):
+    """simple version of solve. one row only"""
+    col = 0
+    while col < 10:    
+        if not puzzle[0][col]:
+            for val in range(1, 10):
+                puzzle[0][col] = val
+                if check_row(0, puzzle):
+                    break
+            else:
+                pass
+        col += 1
+    return puzzle[0]
 
 
 def test():
