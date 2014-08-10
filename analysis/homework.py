fb22fe7e179ba43ca5106bdf946474965e67b515
azizharrington <azizharrington@gmail.com>
2014-08-10 10:17:06 -0500

    dict/list del comparison

--- analysis/homework.py
+++ analysis/homework.py
@@ -7,7 +7,7 @@ for i in [1000, 10000, 100000, 1000000]:
                     "from __main__ import a_list, random")
     a_list = list(range(i))
     time = t.timeit(1000)
-    print("list size", i, time)
+    print("list index", i, time)
 
 
 # test get/set are O(1) for dicts
@@ -20,4 +20,18 @@ for i in [1000, 10000, 100000, 1000000]:
                     "from __main__ import a_dict, random")
     a_dict = {x:None for x in range(i)}
     set_time = t.timeit(1000)
-    print("dict size", i, get_time, set_time)
+    print("dict get/set", i, get_time, set_time)
+
+
+# compare del performance for lists/dicts
+for i in [1000, 10000, 100000, 1000000]:
+    t = timeit.Timer("del(a[random.randrange(len(a))])",
+                    "from __main__ import a, random")
+    a = list(range(i))
+    list_time = t.timeit(1000)
+
+    t = timeit.Timer("del(a[a.keys()[0]])",
+                    "from __main__ import a, random")
+    a = {x:None for x in range(i)}
+    dict_time = t.timeit(1000)
+    print("dict vs list, del", i, list_time, dict_time)
