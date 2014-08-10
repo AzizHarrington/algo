c24f86abcd83fd7a9161378acc6a3fa432ceb588
azizharrington <azizharrington@gmail.com>
2014-08-09 23:18:40 -0500

    sudoku solver working, just barely

--- introduction/sudoku.py
+++ introduction/sudoku.py
@@ -2,18 +2,6 @@ import itertools, sys
 
 sys.setrecursionlimit(5000)
 
-puzzle = [
-    [1, 5, 9, 0, 6, 0, 7, 0, 0],
-    [0, 0, 7, 1, 0, 0, 4, 9, 0],
-    [0, 8, 4, 0, 9, 0, 0, 0, 0],
-    [0, 0, 5, 9, 0, 0, 0, 8, 0],
-    [8, 0, 2, 0, 3, 0, 9, 0, 4],
-    [0, 3, 0, 0, 0, 2, 5, 0, 0],
-    [0, 0, 0, 0, 5, 0, 1, 4, 0],
-    [0, 9, 6, 0, 0, 3, 8, 0, 0],
-    [0, 0, 8, 0, 4, 0, 6, 3, 2]
-]
-
 
 def check_row(row, puzzle):
     digits = [n for n in puzzle[row] if n]
@@ -39,6 +27,20 @@ def check_square(square, puzzle):
     return len(digits) == len(set(digits))
 
 
+def check_all_squares(puzzle):
+    for s in range(9):
+        if not check_square(s, puzzle):
+            return False
+    return True
+
+
+def find_next_blank(puzzle):
+    for row in range(9):
+        for col in range(9):
+            if puzzle[row][col] == 0:
+                return(row, col)
+
+
 def is_solved(puzzle):
     for row in range(len(puzzle)):
         if not (check_row(row, puzzle) and sum(puzzle[row]) == 45):
@@ -52,40 +54,49 @@ def is_solved(puzzle):
             return False
     return True
 
-print(is_solved(puzzle))
+
+puzzle = [
+    [0, 0, 8, 0, 0, 9, 0, 0, 0],
+    [0, 0, 4, 1, 6, 3, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 7, 5],
+    [1, 4, 0, 0, 0, 0, 0, 9, 0],
+    [0, 2, 0, 0, 0, 0, 0, 5, 0],
+    [0, 5, 0, 0, 0, 0, 0, 8, 6],
+    [3, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 7, 8, 1, 9, 0, 0],
+    [0, 0, 0, 3, 0, 0, 4, 0, 0]
+]
 
 
 def solve(puzzle, times=0):
     times += 1
-    print(puzzle[0], times)
 
     if times == 100:
         print("reached limit")
-        return
+        return True
 
-    # if is_solved(puzzle[0]):
-    #     return puzzle[0]
-    solved = True
-    if not (check_row(0, puzzle) and sum(puzzle[0]) == 45):
-        solved = False
-
-    if solved:
-        return puzzle[0]
+    if is_solved(puzzle):
+        return puzzle
 
     else:
-        for cell in range(0, len(puzzle[0])):
-            if (puzzle[0][cell] == 0):
-                for i in range(1, 9):
-                    puzzle[0][cell] += 1
-                    if check_row(0, puzzle) and check_column(cell, puzzle):
-                        if solve_row1(puzzle, times):
-                            return solve_row1(puzzle, times)
-                        else:
-                            puzzle[0][cell] = 0
-    return None 
+        row, col = find_next_blank(puzzle)
+        for i in range(1, 10):
+            puzzle[row][col] = i
+            if check_row(row, puzzle) and check_column(col, puzzle) and check_all_squares(puzzle):
+                
+                for r in puzzle:
+                    print(r)
+                print(times)
 
+                if solve(puzzle, times):
+                    return True
+                else:
+                    puzzle[row][col] = 0
+        else:
+            puzzle[row][col] = 0
+    return False
 
-print(solve_row1(puzzle))
+print(solve(puzzle))
 
 
 def test():
@@ -109,4 +120,3 @@ def test():
     assert check_square(0, test_puzzle) == False
 
     print('tests passed')
-
