f0b7c1fa76e77a73956c2b8792565048ccfabac8
azizharrington <azizharrington@gmail.com>
2014-08-11 13:22:07 -0500

    parenthesis checker

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -33,7 +33,7 @@ def test():
     assert s.pop() == True
     assert s.size() == 2
 
-    print("tests pass")
+    print("stack tests pass")
 
 
 test()
@@ -47,3 +47,30 @@ def revstring(a_str):
     while not s.is_empty():
         revstr += s.pop()
     return revstr
+
+
+def par_checker(symbol_str):
+    """takes string with parenthesis and returns true if balanced"""
+    s = Stack()
+    for sym in symbol_str:
+        if sym == '(':
+            s.push(sym)
+        elif sym == ')':
+            try:
+                s.pop()
+            except:
+                return False
+    return s.size() == 0
+
+
+def test_par_checker():
+    str1 = "((1 + 2) * (3 / 4) / (2 ** 2) + ( 1 + 1))"
+    str2 = "(((((((True) or  False)"
+
+    assert par_checker(str1) == True
+    assert par_checker(str2) == False
+
+    print("par_checker tests pass")
+
+
+test_par_checker()
