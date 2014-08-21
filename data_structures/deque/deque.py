d495d1f5307e23489cb374e3957104b809f945c0
azizharrington <azizharrington@gmail.com>
2014-08-21 16:08:48 -0500

    deque class

---
+++ data_structures/deque/deque.py
@@ -0,0 +1,47 @@
+class Deque:
+    def __init__(self):
+        self.items = []
+
+    def add_front(self, item):
+        self.items.insert(0, item)
+
+    def add_rear(self, item):
+        self.items.append(item)
+
+    def remove_front(self):
+        return self.items.pop(0)
+
+    def remove_rear(self):
+        return self.items.pop()
+
+    def is_empy(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
+
+
+def test():
+    d = Deque()
+
+    assert d.is_empy() == True
+
+    d.add_rear(4)
+    d.add_rear('dog')
+    d.add_front('cat')
+    d.add_front(True)
+
+    assert d.size() == 4
+    assert d.is_empy() == False
+
+    d.add_rear(8.4)
+
+    assert d.remove_rear() == 8.4
+    assert d.remove_front() == True
+
+    print("tests pass")
+
+
+if __name__ == '__main__':
+
+    test()
