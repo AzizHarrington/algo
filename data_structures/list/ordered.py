4b1780fe7be912f861a0c4eea4a1aedd544d18d8
azizharrington <azizharrington@gmail.com>
2014-08-26 12:51:01 -0500

    ordered list api

---
+++ data_structures/list/ordered.py
@@ -0,0 +1,24 @@
+class OrderedList:
+    def __init__(self):
+        pass
+
+    def add(self, item):
+        pass
+
+    def remove(self, item):
+        pass
+
+    def search(self, item):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def size(self):
+        pass
+
+    def index(self, item):
+        pass
+
+    def pop(self, pos):
+        pass
