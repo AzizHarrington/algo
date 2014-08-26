87e11ced6821f63729c4c81e22f9e361e77621f8
azizharrington <azizharrington@gmail.com>
2014-08-26 10:11:30 -0500

    finished list methods

--- data_structures/list/unordered.py
+++ data_structures/list/unordered.py
@@ -42,16 +42,71 @@ class UnorderedList:
             prev, current = current, current.get_next()
 
     def index(self, item):
-        pass
+        current = self.head
+        index = -1
+        while current:
+            index += 1
+            if current.get_data() == item:
+                return index
+            current = current.get_next()
+        return -1
 
     def append(self, item):
-        pass
+        new_node = Node(item)
+        current = self.head
+        while current:
+            # at the end of the list
+            if not current.get_next():
+                current.set_next(new_node)
+                break
+            current = current.get_next()
+        else:
+            # empty list, so set as first item
+            self.head = new_node
 
     def insert(self, position, item):
-        pass
-
-    def pop(self, position=0):
-        pass
+        new_node = Node(item)
+        current = self.head
+        prev = None
+        current_pos = 0
+        while current:
+            if current_pos == position:
+                if not prev:
+                    # at the beginning of the list,
+                    # so link new node to current head
+                    new_node.set_next(current)
+                    # and assign new node to list head
+                    self.head = new_node
+                else:
+                    # insert new_node
+                    new_node.set_next(current)
+                    prev.set_next(new_node)
+                break
+            prev, current = current, current.get_next()
+            current_pos += 1
+        else:
+            self.append(item)
+                
+    def pop(self, position=None):
+        current = self.head
+        prev = None
+        current_pos = 0
+        while current:
+            if current_pos == position:
+                if not prev:
+                    # at head, so reassign to next node
+                    self.head = current.get_next()
+                else:
+                    # break link to current
+                    temp = current.get_next()
+                    prev.set_next(temp)
+                # finally return current
+                return current.get_data()
+            elif not position and not current.get_next():
+                prev.set_next(None)
+                return current.get_data()
+            prev, current = current, current.get_next()
+            current_pos += 1
 
 
 def test():
@@ -73,18 +128,36 @@ def test():
 
     assert ul.index("bob") == 1
     assert ul.index(2) == 2
+    assert ul.index("frank") == -1
 
     ul.append("pizza")
-    assert ul.search("pizza")
+    assert ul.search("pizza") == True
     assert ul.size() == 4
+
+    ul2 = UnorderedList()
+
+    ul2.append("hello world")
+    assert ul2.size() == 1
 
     ul.insert(0, 4.5)
     assert ul.index(4.5) == 0
     assert ul.size() == 5
 
-    assert ul.pop() == 2
-    assert ul.pop(0) == 4.5
-    assert ul.size() == 3
+    ul.insert(2, 100)
+    assert ul.index(100) == 2
+    assert ul.size() == 6
+
+    ul3 = UnorderedList()
+
+    ul3.insert(0, 3)
+    assert ul3.size() == 1
+
+    ul3.add(2)
+    ul3.add("hello")
+    ul3.add(True)
+    assert ul3.pop() == 3
+    assert ul3.pop(0) == True
+    assert ul3.size() == 2
 
     print("tests pass")
 
