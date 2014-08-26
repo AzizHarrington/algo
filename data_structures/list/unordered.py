fb2417e3cf26b231a43838ea20816ce0f987893d
azizharrington <azizharrington@gmail.com>
2014-08-25 23:00:19 -0500

    tests for remaining list methods

--- data_structures/list/unordered.py
+++ data_structures/list/unordered.py
@@ -1,5 +1,6 @@
 from node import Node
 
+
 class UnorderedList:
     def __init__(self):
         self.head = None
@@ -38,14 +39,25 @@ class UnorderedList:
                 else:
                     prev.set_next(current.get_next())
                 break
-            prev, current = current, current.get_next()         
+            prev, current = current, current.get_next()
+
+    def index(self, item):
+        pass
+
+    def append(self, item):
+        pass
+
+    def insert(self, position, item):
+        pass
+
+    def pop(self, position=0):
+        pass
 
 
 def test():
     ul = UnorderedList()
 
     assert ul.size() == 0
-
     ul.add(2)
     ul.add(1)
     assert ul.size() == 2
@@ -56,6 +68,22 @@ def test():
     assert ul.search("dog") == False 
 
     ul.remove(1)
+    assert ul.search(1) == False
+    assert ul.size() == 3
+
+    assert ul.index("bob") == 1
+    assert ul.index(2) == 2
+
+    ul.append("pizza")
+    assert ul.search("pizza")
+    assert ul.size() == 4
+
+    ul.insert(0, 4.5)
+    assert ul.index(4.5) == 0
+    assert ul.size() == 5
+
+    assert ul.pop() == 2
+    assert ul.pop(0) == 4.5
     assert ul.size() == 3
 
     print("tests pass")
