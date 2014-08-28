122cf386ff4615b91dd4423dd388194b648ce310
azizharrington <azizharrington@gmail.com>
2014-08-28 18:23:08 -0500

    extended to hexidecimal converions

--- recursion/int2basestring.py
+++ recursion/int2basestring.py
@@ -1,15 +1,18 @@
 
 def int2base(integer, base):
-    digits = "0123456789"
-    if integer < 10:
+    digits = "0123456789ABCDEF"
+    if integer < base:
         return digits[integer]
     return int2base((integer/base), base) + int2base((integer%base), base)
 
 
 def test():
     num1 = 769
+    num2 = 1453
 
     assert int2base(num1, 10) == "769"
+
+    assert int2base(num2, 16) == "5AD"
 
     print "tests passed"
 
