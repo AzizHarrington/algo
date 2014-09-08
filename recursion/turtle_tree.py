2ba1d84b3b21735334813729b66af0ba4d30cb1f
azizharrington <azizharrington@gmail.com>
2014-09-08 09:26:18 -0500

    playing with the algorithm, added style

--- recursion/turtle_tree.py
+++ recursion/turtle_tree.py
@@ -1,15 +1,36 @@
 import turtle
-
+from random import randint
 
 def tree(branch_len, t):
     if branch_len > 5:
+        style_pen(branch_len, t)
+        t.pendown()
         t.forward(branch_len)
         t.right(20)
-        tree(branch_len-15, t)
+        tree(branch_len-randint(5, 15), t)
         t.left(40)
-        tree(branch_len-15, t)
+        tree(branch_len-randint(5, 25), t)
         t.right(20)
+        t.penup()
         t.backward(branch_len)
+
+
+def style_pen(branch_len, t):
+    if branch_len > 75:
+        t.pensize(10)
+        t.color("#663300")
+    elif branch_len > 50:
+        t.pensize(7)
+        t.color("#996633")
+    elif branch_len > 25:
+        t.pensize(4)
+        t.color("#cccc00")
+    elif branch_len > 10:
+        t.pensize(1)
+        t.color("#009900")
+    else:
+        t.pensize(5)
+        t.color("#006600")
 
 
 def main():
@@ -17,10 +38,9 @@ def main():
     my_win = turtle.Screen()
     t.left(90)
     t.up()
-    t.backward(150)
+    t.backward(200)
     t.down()
-    t.color("green")
-    tree(150, t)
+    tree(100, t)
     my_win.exitonclick()
 
 
