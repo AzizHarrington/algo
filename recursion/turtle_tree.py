96cc29d138f894dc8c0ed360ebe67b59abc8b2e8
azizharrington <azizharrington@gmail.com>
2014-09-08 14:22:44 -0500

    more randomness

--- recursion/turtle_tree.py
+++ recursion/turtle_tree.py
@@ -5,14 +5,18 @@ def tree(branch_len, t):
     if branch_len > 5:
         style_pen(branch_len, t)
         t.pendown()
-        t.forward(branch_len)
-        t.right(20)
+        # randomly set branch len
+        forward_distance = randint(branch_len-10, branch_len+10)
+        t.forward(forward_distance)
+        # randomly set angle
+        angle = randint(10, 20)
+        t.right(angle)
         tree(branch_len-randint(5, 15), t)
-        t.left(40)
+        t.left(angle*2)
         tree(branch_len-randint(5, 25), t)
-        t.right(20)
+        t.right(angle)
         t.penup()
-        t.backward(branch_len)
+        t.backward(forward_distance)
 
 
 def style_pen(branch_len, t):
@@ -38,7 +42,7 @@ def main():
     my_win = turtle.Screen()
     t.left(90)
     t.up()
-    t.backward(200)
+    t.backward(400)
     t.down()
     tree(100, t)
     my_win.exitonclick()
