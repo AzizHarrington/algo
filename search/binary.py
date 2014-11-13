b2ba868ef573c80dccf7a99d08bf79c2f58498ee
azizharrington <azizharrington@gmail.com>
2014-11-13 09:39:08 -0600

    recursive binary ordered search

--- search/binary.py
+++ search/binary.py
@@ -2,7 +2,7 @@ def binary_search(alist, item):
     found = False
 
     while not found and len(alist) > 0:
-        mid_point = len(alist) / 2
+        mid_point = len(alist) // 2
         if alist[mid_point] == item:
             found = True
         elif alist[mid_point] < item:
@@ -13,14 +13,36 @@ def binary_search(alist, item):
     return found
 
 
+def recursive_binary_search(alist, item):
+    if not alist:
+        return False
+    mid_point = len(alist) // 2
+    if alist[mid_point] == item:
+        return True
+    elif alist[mid_point] > item:
+        return recursive_binary_search(alist[:mid_point], item)
+    elif alist[mid_point] < item:
+        return recursive_binary_search(alist[mid_point+1:], item)
+
+
 test_list = [1, 2, 6, 9, 10, 11, 17, 40, 45]
 
-print(binary_search(test_list, 17))
-print(binary_search(test_list, 6))
-print(binary_search(test_list, 1))
-print(binary_search(test_list, 45))
-print(binary_search(test_list, 15))
+
+def test():
+    assert binary_search(test_list, 17)
+    assert binary_search(test_list, 6)
+    assert binary_search(test_list, 1)
+    assert binary_search(test_list, 45)
+    assert not binary_search(test_list, 15)
+
+    assert recursive_binary_search(test_list, 17)
+    assert recursive_binary_search(test_list, 6)
+    assert recursive_binary_search(test_list, 1)
+    assert recursive_binary_search(test_list, 45)
+    assert not recursive_binary_search(test_list, 15)
+
+    print("tests pass")
 
 
-def recursive_binary_search(alist, item):
-    pass
+if __name__ == "__main__":
+    test()
