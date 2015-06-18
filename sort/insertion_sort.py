2047cf50b26c543663b2a04776ad9e1dde656368
Aziz Harrington <aharrington@leapfrogonline.com>
2015-06-18 12:19:19 -0500

    test runtime of insertion sort

--- sort/insertion_sort.py
+++ sort/insertion_sort.py
@@ -1,3 +1,6 @@
+from helpers import test_runtime
+
+
 def insertion_sort(a_list):
     for index, item in enumerate(a_list):
         for x, comparison in enumerate(a_list[index::-1]):
@@ -14,19 +17,10 @@ def test():
     assert insertion_sort([]) == []
     assert insertion_sort([2, 4, 2, 4]) == [2, 2, 4, 4]
     assert insertion_sort([1, 2, 3]) == [1, 2, 3]
+    assert test_runtime(1000, insertion_sort) < 1.0  # seconds
     print("insertion sort tests passed")
-
-def test_runtime(size):
-    import random
-    l = list(range(size))
-    random.shuffle(l)
-    import time
-    start = time.time()
-    insertion_sort(l)
-    end = time.time() - start
-    print("List of {} elements took {} seconds to sort".format(size, end))
 
 
 if __name__ == "__main__":
 
-    test_runtime(10000)
+    test()
